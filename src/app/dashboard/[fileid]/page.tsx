a49cf069b82c6c0d405f19b1beae131f97e25bd9
aneeshkandal <kandal.aneesh25@gmail.com>
2023-10-12 22:47:38 +0530

    ChatInput

--- src/app/dashboard/[fileid]/page.tsx
+++ src/app/dashboard/[fileid]/page.tsx
@@ -3,7 +3,7 @@ import { db } from '@/db'
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
 import { notFound, redirect } from 'next/navigation'
 import PdfRenderer from '@/components/PdfRenderer'
-import ChatWrapper from '@/components/ChatWrapper'
+import ChatWrapper from '@/components/chat/ChatWrapper'
 
 interface PageProps {
     params: {
@@ -37,12 +37,12 @@ const page = async ({ params }: PageProps) => {
             <div className='flex-1 xl:flex'>
                 <div className='px-4 py-6 sm:px-6 lg:pl-8 xl:flex-1 xl:pl-6'>
                     {/* Main area */}
-                    <PdfRenderer />
+                    <PdfRenderer url={file.url}/>
                 </div>
             </div>
 
             <div className='shrink-0 flex-[0.75] border-t border-gray-200 lg:w-96 lg:border-l lg:border-t-0'>
-                    <ChatWrapper />
+                    <ChatWrapper fileId={file.id}  />
             </div>
         </div>
     </div>
