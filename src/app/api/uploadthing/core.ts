e3541b7954239bc1c333d2b07ef544af2355cb8c
aneeshkandal <kandal.aneesh25@gmail.com>
2023-10-17 22:54:34 +0530

    partial work

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -3,7 +3,7 @@ import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 import { PDFLoader } from 'langchain/document_loaders/fs/pdf'
 import { PineconeStore } from 'langchain/vectorstores/pinecone'
-import { pinecone } from "@/lib/pinecone";
+import { pinecone } from '@/lib/pinecone'
 import { OpenAIEmbeddings } from 'langchain/embeddings/openai'
  
 const f = createUploadthing();
@@ -37,30 +37,20 @@ export const ourFileRouter = {
 
         const blob = await response.blob()
         const loader = new PDFLoader(blob)
-        const pageLevelDocs = (await loader.load()).map((doc) => {
-          return {
-            ...doc,
-            metadata: {
-              ...doc.metadata,
-              "file.id": createdFile.id,
-            },
-          };
-        });
+        const pageLevelDocs = await loader.load()
         
         const pagesAmt = pageLevelDocs.length
 
          // vectorize and index entire document
-   
-         const pineconeIndex = pinecone
-           .Index("docuquest")
-           .namespace(metadata.userId);
-         
+        const pineconeIndex = pinecone.Index('docuquest')
+
         const embeddings = new OpenAIEmbeddings({
           openAIApiKey: process.env.OPENAI_API_KEY,
         })
 
         await PineconeStore.fromDocuments(pageLevelDocs, embeddings, {
           pineconeIndex,
+         namespace: createdFile.id,
         });
 
         
